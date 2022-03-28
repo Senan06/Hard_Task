35fc88d4e23851e6607d32b024bc521acab89c51
Senan06 <99797322+Senan06@users.noreply.github.com>
2022-03-28 12:42:13 +0400

    dgdfgdgf
    dgdfgdgf

---
+++ HardTask/HardTask/Models/Group.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HardTask.Models
+{
+    class Group
+    {
+        private static int _no=100;
+        public string No 
+        {
+            get
+            {
+                return $"AP{_no}";
+            }        
+        }
+        public Student[] students;
+        public void AddStudent(Student student)
+        {
+            Array.Resize(ref students, students.Length + 1);
+            students[students.Length - 1] = student;            
+        }
+        public string GetAllStudents(Student[] studentsArr)
+        {
+            foreach (Student item in studentsArr)
+            {
+                return item.Showinfo();
+            }
+            return "";
+        }
+        public Student[] Sort()
+        {
+            Student[] inputStudents = new Student[students.Length];
+            Student 
+            for(int i=0 ; i<inputStudents.Length; i++)
+            {
+                for (int j = i; j < students.Length; j++)
+                {
+                    
+                }
+            }
+            return inputStudents;
+        }
+    }
+}
---
+++ HardTask/HardTask/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HardTask
+{
+    public class Student
+    {
+        private static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Point { get; set; }
+        public string Showinfo()
+        {
+            return $" ";
+        }
+        public static bool operator >(Student student, Student student2)
+        {
+            return student.Point > student2.Point;
+        }
+        public static bool operator <(Student student, Student student2)
+        {
+            return student.Point < student2.Point;
+        }
+
+    }
+}
